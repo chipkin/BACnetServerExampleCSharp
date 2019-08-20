addb5bb9e23a58048153403a434ec9ea869d54d4
Steven Smethurst <funvill@funvill.com>
2019-08-20 11:08:47 -0700

    Changed Adapter name space (#5)
    Changed Adapter name space

--- BACnetServerExample/BACnetServerExample/CIBuildVersion.cs
+++
@@ -1,2 +0,0 @@
-// This file is overwritten during the CI Build phase. 
-namespace BACnetServerExample { static class CIBuildVersion { public const int CIBUILDNUMBER = 0; } }
---
+++ BACnetServerExample/CIBuildVersion.cs
@@ -0,0 +1,2 @@
+// This file is overwritten during the CI Build phase. 
+namespace CASBACnetStack { static class CIBuildVersion { public const int CIBUILDNUMBER = 0; } }
