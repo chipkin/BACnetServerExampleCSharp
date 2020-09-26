73e5383e6052d5209c679c36c924e116d36e9ffe
mochiman <jmcdude56@hotmail.com>
2020-09-25 17:07:13 -0700

    Version update (#8)
    * Updated CAS BACnet Stack to version 3.19.0.x
    
    * Updated version number

--- BACnetServerExample/Program.cs
+++ BACnetServerExample/Program.cs
@@ -49,7 +49,7 @@ namespace BACnetServerExample
             private ExampleDatabase database = new ExampleDatabase();
 
             // Version 
-            const string APPLICATION_VERSION = "0.0.3";
+            const string APPLICATION_VERSION = "0.0.4";
 
             // Server setup and main loop
             public void Run()
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## Version 0.0.x
 
+### 0.0.4 (2020-Sep-25)
+
+- Updated to CAS BACnet stack version 3.19.0.x
+
 ### 0.0.3 (2020-Aug-19)
 
 - Added example of setting the protocol revision.
